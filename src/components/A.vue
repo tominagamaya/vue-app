d19497ed6a3e4586c04377681f045aed87a8a9a3
tominagamaya <maya.tominaga.9@gmail.com>
2018-08-12 21:06:18 +0900

    add vuex for counter

--- src/components/A.vue
+++ src/components/A.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="container">
-    <h1>{{ msg }}</h1>
+  <div>
+    <button @click="countUp">add count</button>
+    <div>{{currentCount}}</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'A',
-  data() {
-    return {
-      msg: 'Hello A!!',
-    };
-  },
-};
+  export default {
+    computed: {
+      currentCount() {
+        return this.$store.getters.currentCount
+      }
+    },
+    methods: {
+      countUp() {
+        this.$store.dispatch("plusCount")
+      }
+    }
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
+import store from './store/store'
+import { sync } from 'vuex-router-sync'
 
 Vue.use(VueRouter)
 
@@ -9,8 +11,11 @@ const router = new VueRouter({
   routes
 })
 
+sync(store, router)
+
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
